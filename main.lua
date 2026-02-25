075c0253c1c6bb0a1032a6106e2115126d683aba
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2026-02-25 22:18:34 +0900

    feat: include server.lua (wip)

--- main.lua
+++ main.lua
@@ -1,5 +1,17 @@
-if lovr then
-	require "lovr_main"
-else
-	require "love_main"
+local sep = package.config:sub(1,1)
+function join_path(a, b)
+	return a .. sep .. b
 end
+
+local base_path
+if lovr then
+	base_path = lovr.filesystem.getSource()
+else
+	base_path = love.filesystem.getSource()
+end
+
+print(base_path)
+local f = io.open(join_path(base_path, "server.lua"))
+local s = f:read '*a'
+f:close()
+print(s)
