3a6dd2ff1db130f360a53957e7f29881cc55cd0f
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2026-02-27 05:47:18 +0900

    feat: echo server

--- main.lua
+++ main.lua
@@ -1,7 +1,49 @@
-if lovr then
-	server_th = lovr.thread.newThread("server.lua")
-else
-	server_th = love.thread.newThread("server.lua")
+local server_thread
+
+function server_start()
+	if lovr then
+		server_thread = lovr.thread.newThread("server.lua")
+	else
+		server_thread = love.thread.newThread("server.lua")
+	end
+	server_thread:start()
 end
 
-server_th:start()
+----
+
+local t = 0
+
+-- load
+if lovr then
+	function lovr.load()
+		server_start()
+	end
+else
+	function love.load()
+		server_start()
+	end
+end
+
+-- update
+if lovr then
+	function lovr.update(dt)
+		t = t + dt
+	end
+else
+	function love.update(dt)
+		t = t + dt
+	end
+end
+
+-- draw
+if lovr then
+	function lovr.draw(pass)
+		pass:text('hello world', 0, 1.7, -5, 1, t)
+	end
+else
+	function love.draw()
+		x = math.sin(t) * 100 + 100
+		y = math.cos(t) * 100 + 100
+		love.graphics.rectangle("fill", x, y, 30, 30)
+	end
+end
--- server.lua
+++ server.lua
@@ -1,1 +1,29 @@
-print("hello. I'm a server.")
+local socket = require("socket")
+local server = assert(socket.bind("*", 0))
+-- find out which port the OS chose for us
+local ip, port = server:getsockname()
+-- print a message informing what's up
+print("Please telnet to localhost on port " .. port)
+print("After connecting, you have 10s to enter a line to be echoed")
+
+-- loop forever waiting for clients
+while true do
+    -- wait for a connection from any client
+    local client = server:accept()
+    print("New client connected")
+    -- make sure we don't block waiting for this client's line
+    client:settimeout(10)
+    for i = 1, 3 do
+        client:send("\r> ")
+        -- receive the line
+        local line, err = client:receive()
+        -- if there was no error, send it back to the client
+        if not err then client:send(line .. "\n") end
+            -- done with client, close the object
+            -- client:close()
+    end
+
+    client:send("Echo end. closed.")
+    client:close()
+    print("Closed. Waiting next client.")
+end
